6ac93780b4f7c0d28cbad152434470f8c08fc835
luisgs <luis.ild@gmail.com>
2010-11-12 18:06:46 +0000

    confirmado

--- compilador_code/Proyecto/src/analizador/Compilador.java
+++ compilador_code/Proyecto/src/analizador/Compilador.java
@@ -6,21 +6,18 @@ import antlr.ANTLRException;
 import antlr.Token;
 import antlr.TokenStreamException;
 
-public interface Compilador {
+public class Compilador {
 	
-	public static void main(String args[]) throws TokenStreamException {
+	public static void main (String args[]) throws TokenStreamException {
 		try {
-		   	//String linea = leer_linea(args);	// recibimos una linea
 	        System.out.println("Empieza a dar tokens:");
 			FileInputStream fis = new FileInputStream("./src/analizador/cod_fuente1");
 			analizador Analizador = new analizador(fis);
-			Analizador  token = Analizador.nextToken();
+			Token  token = Analizador.nextToken();
 			while(token.getType() != Token.EOF_TYPE) {
 				System.out.println(token);
 				token = Analizador.nextToken();
 			}
-
-
 		}catch(FileNotFoundException fnfe) {
 			System.err.println("No se encontró el fichero");
 		}
