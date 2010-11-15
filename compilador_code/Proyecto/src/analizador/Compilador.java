3c45f8e2f0025af29e5224f69969e12d253c037d
luisgs <luis.ild@gmail.com>
2010-11-15 18:46:37 +0000

    Main modificado. Ahora en teoría no debe romper. Falta devolver línea del Token erróneo

--- compilador_code/Proyecto/src/analizador/Compilador.java
+++ compilador_code/Proyecto/src/analizador/Compilador.java
@@ -1,30 +1,38 @@
 package analizador;
 import java.io.*;
 //import org.antlr.runtime.CommonTokenStream;
-//import antlr.collections.AST;
-//import antlr.ANTLRException;
+import antlr.collections.AST;
+import antlr.ANTLRException;
+import antlr.RecognitionException;
 import antlr.Token;
 import antlr.TokenStreamException;
 
 public class Compilador {
 	
-	public static void main (String args[]) throws TokenStreamException {
+	public static void main (String args[])  {
 		try {
-	        System.out.println("Empieza a dar tokens:");
 	        String ext =args[0].substring(args[0].indexOf("."));
 	        if(ext.equalsIgnoreCase(".cpp")) {
+		        System.out.println("Escribiendo tokens fichero:");
 				FileInputStream fis = new FileInputStream(args[0]);
 				analizador Analizador = new analizador(fis);
-				Token token = Analizador.nextToken();
-				while(token.getType() != Token.EOF_TYPE) {
-					System.out.println(token);
-					token = Analizador.nextToken();
+				try {
+					Token token = Analizador.nextToken();
+					while(token.getType() != Token.EOF_TYPE) {
+						System.out.println(token);
+						token = Analizador.nextToken();
+					}
+				} catch (ANTLRException Ex) {				
+						System.err.println("Error en token desconocido.");
+						System.err.println(Ex.getMessage());
 				}
 	        } else {
 				System.err.println("No es un fichero con extensión correcta .cpp");
 	        }
-		}catch(FileNotFoundException fnfe) {
-			System.err.println("No se encontró el fichero");
+		} catch(FileNotFoundException fnfe) {
+			System.err.println("No se encontró el fichero especificado.");
 		}
+		
+
 	}
 }
