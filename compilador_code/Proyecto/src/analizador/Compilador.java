696505519befb4aec0ad1752bbf761346827a741
luisgs <luis.ild@gmail.com>
2010-11-16 17:13:22 +0000

    Main mejorado

--- compilador_code/Proyecto/src/analizador/Compilador.java
+++ compilador_code/Proyecto/src/analizador/Compilador.java
@@ -9,30 +9,32 @@ import antlr.TokenStreamException;
 
 public class Compilador {
 	
-	public static void main (String args[])  {
+	public static void main (String args[])   {
 		try {
-	        String ext =args[0].substring(args[0].indexOf("."));
-	        if(ext.equalsIgnoreCase(".cpp")) {
-		        System.out.println("Escribiendo tokens fichero:");
-				FileInputStream fis = new FileInputStream(args[0]);
-				analizador Analizador = new analizador(fis);
-				try {
-					Token token = Analizador.nextToken();
-					while(token.getType() != Token.EOF_TYPE) {
-						System.out.println(token);
-						token = Analizador.nextToken();
+			if (args.length>=1) {
+		        String ext = args[0].substring(args[0].indexOf("."));
+		        if(ext.equalsIgnoreCase(".cpp")) {
+			        System.out.println("Escribiendo tokens fichero:");
+					FileInputStream fis = new FileInputStream(args[0]);
+					analizador Analizador = new analizador(fis);
+					try {
+						Token token = Analizador.nextToken();
+						while(token.getType() != Token.EOF_TYPE) {
+							System.out.println(token);
+							token = Analizador.nextToken();
+						}
+					} catch (ANTLRException Ex) {				
+							System.err.println("Error en token desconocido.");
+							System.err.println(Ex.getMessage());
 					}
-				} catch (ANTLRException Ex) {				
-						System.err.println("Error en token desconocido.");
-						System.err.println(Ex.getMessage());
-				}
-	        } else {
-				System.err.println("No es un fichero con extensión correcta .cpp");
-	        }
+		        } else {
+					System.err.println("No es un fichero con extensión correcta .cpp");
+		        }
+			} else {
+				System.err.println("No ha introducido ningún argumento [fichero fuente con extensión .cpp]");
+			}
 		} catch(FileNotFoundException fnfe) {
 			System.err.println("No se encontró el fichero especificado.");
 		}
-		
-
 	}
 }
