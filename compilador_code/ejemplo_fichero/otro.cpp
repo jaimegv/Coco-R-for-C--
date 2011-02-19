d517101dd0db62f0017d5f5853b07b1dc1f9c02f
luisgs <luis.ild@gmail.com>
2011-02-19 17:17:43 +0000

    Nuestro sintáctico reconoce actualmente este fichero.

--- compilador_code/ejemplo_fichero/otro.cpp
+++ compilador_code/ejemplo_fichero/otro.cpp
@@ -1,8 +1,18 @@
 // hola
 /*asfdsdf
-Adasd*/
-void hola (char * er, int qqwe, int qwe) {
+Adasd
+void hola (char er, int qqwe, int qwe) {
 	int qwe, jarl = 123, HOLA, _asd, jarl=asd, ere[23];
 	yo.imprime ();
 	ella.imprime (as);
+}
+*/
+int GLOBAL=10;
+
+void main (void)	// función principal
+{ 	
+int qwe, jarl = 123, HOLA, _asd, jarl=asd, cancion = lista(23);
+//Persona Luis;
+bool paco=0123, HOLA; int paco2;
+
 }
