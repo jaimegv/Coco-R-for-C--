f3617b9cfa8bbd396bf3248ed79438e217691c08
luisgs <luis.ild@gmail.com>
2011-03-05 17:25:05 +0000

    Modificado main Compilador.java, ahora pasa el lexico y si error en este se detieney da error, eoc sigue con el sintactico.

--- compilador_code/Proyecto/src/analizador/Compilador.java
+++ compilador_code/Proyecto/src/analizador/Compilador.java
@@ -17,40 +17,41 @@ public class Compilador {
 				int longitud = args[0].length();
 		        String ext = args[0].substring(longitud - 4);
 		        if(ext.equalsIgnoreCase(".cpp")) {
-/*		        	if (args.length < 2) {
+		        	if (args.length < 2) {
 			        		System.err.println("Introduzca fichero destino");
 		        	} else {
 			        	PrintStream fichero = new PrintStream(new File(args[1]));
-			        	*/
 		        		// Creo el analizador lexico
 						FileInputStream fis = new FileInputStream(args[0]);
 						analizador Analizador = new analizador(fis);
-						Analizador.setFilename(args[0]);
-						Analizador.setTokenObjectClass("antlr.CommonToken");
-						// Para el analizador Sintáctico
-						// para el constructor
-						//CommoTokenStream tokens = new CommonTokenStream(Analizador);
-						CompParser Parser = new CompParser(Analizador);
-						Parser.setFilename(args[0]);
-						Parser.programa();
-						AST ast = Parser.getAST();
-
-						System.out.println(ast.toStringList());						
-						/*try {
+					
+						try {	// del analizador lexico
 							Token token = Analizador.nextToken();
 							while(token.getType() != Token.EOF_TYPE) {
-//								fichero.println(token);
-								System.out.println("juas juas"+token);
+								fichero.println(token);
+								// System.out.println("Léxico:"+token);
 								token = Analizador.nextToken();
 							}
+							
+							// Del analizador sintáctico
+							Analizador.setFilename(args[0]);
+							Analizador.setTokenObjectClass("antlr.CommonToken");
+							// Para el analizador Sintáctico
+							// para el constructor
+							//CommoTokenStream tokens = new CommonTokenStream(Analizador);
+							CompParser Parser = new CompParser(Analizador);
+							Parser.setFilename(args[0]);
+							Parser.programa();
+							AST ast = Parser.getAST();					
+							System.out.println(ast.toStringList());	
 						} catch (ANTLRException Ex) {				
 								//fichero.println("Error en token desconocido.");
 								//fichero.println(Ex.getMessage());
-								System.out.println("Error en token desconocido.");
+								System.out.println("Error léxico en carácter desconocido.");
 								System.out.println(Ex.getMessage());
 						}
-*/
-/*					}*/
+
+					}
 		        } else {
 					System.err.println("No es un fichero con extensión correcta .cpp");
 		        }
