fbb11756b9a06347f8984e087bb0eadcff530c5c
luisgs <luis.ild@gmail.com>
2011-03-09 23:11:27 +0000

    Actualizada prueba 2

--- Segunda_entrega/ejecutable/pruebas_correctas/Prueba_2.cpp
+++ Segunda_entrega/ejecutable/pruebas_correctas/Prueba_2.cpp
@@ -1,20 +1,20 @@
-// Prueba Correcta
+// Prueba Correcta 
 
-// declaracion de metodo y clase
-//Declaración de la clase Fecha:
-class Fecha {
-public: int a, b, c;
-	int daDia (void);	// método que devuelve un entero
-private:
-	int d,m,a;	// tres enteros privados
-};
+// declaracion de metodo y clase 
+//Declaración de la clase Fecha: 
+class Fecha { 
+public: int a, b, c; 
+	int daDia (void);	// método que devuelve un entero 
+private: 
+	int d,m,a;	// tres enteros privados 
+}; 
 
-// Definicion de un metodo daDia de la clase Fecha
-int Fecha::daDia (void){
-	return d;
-}
+// Definicion de un metodo daDia de la clase Fecha 
+int Fecha::daDia (void){ 
+	return d; 
+} 
 
-void main (int dia) {
-	Fecha var;
-	return var.daDia();
+void main (int dia) { 
+	Fecha var; 
+	return var.daDia(); 
 }
