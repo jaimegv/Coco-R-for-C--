8200420338122d4bea143cb48bb2ad9cd910e443
luisgs <luis.ild@gmail.com>
2011-08-08 14:58:23 +0000

    Primera prueba del fichero con la tabla de simbolos

---
+++ CocoNuestro/src/compilationunit/TablaSimbolos.java
@@ -0,0 +1,18 @@
+package compilationunit;
+
+import java.util.Vector;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.Queue;
+import java.util.LinkedList;
+
+
+public class TablaSimbolos {
+
+	
+	
+    public void DestruirTSG () {
+    	System.out.println("Hola mundo! esto es la Tabla de simbolo");
+    }
+}
