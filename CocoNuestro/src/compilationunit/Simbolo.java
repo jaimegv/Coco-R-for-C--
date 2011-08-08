be1a6d1416e117f5a4227586fbc62cc87da071de
luisgs <luis.ild@gmail.com>
2011-08-08 17:37:56 +0000

    Modificacion de la tabla de simbolos y añadidura de l objeto simbolo que ira siendo contenido en la tabla, ademas se ha empezado a modificar la gramtica para comprobar tipos

---
+++ CocoNuestro/src/compilationunit/Simbolo.java
@@ -0,0 +1,7 @@
+package compilationunit;
+
+public class Simbolo {
+	String nombre;	// Nombre del objeto
+	int type;		// tipo del objeto
+	
+}
--- CocoNuestro/src/compilationunit/TablaSimbolos.java
+++ CocoNuestro/src/compilationunit/TablaSimbolos.java
@@ -1,15 +1,28 @@
 package compilationunit;
 
-import java.util.Vector;
+/*import java.util.Vector;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Set;
 import java.util.Queue;
 import java.util.LinkedList;
-
+*/
 
 public class TablaSimbolos {
-
+	
+	// Declaracion de constantes para tipos
+	final int undef=0, entera=1, bool=2, cadena=3;
+	// Declaración de constantes de tipo de scopes
+	final int var=0, funcion=1, clase=2, metodo=3;
+	
+	public Simbolo undefObj;
+	
+	
+	public void SymbolTable(Parser parser) {
+		undefObj = new Simbolo();
+		undefObj.nombre  =  "undef"; 
+		undefObj.type = undef; 
+	}
 	
 	
     public void DestruirTSG () {
