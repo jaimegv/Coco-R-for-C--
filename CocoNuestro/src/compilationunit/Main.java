eaadc3505aeecbce54f8f80299b3976c62fe2581
luisgs <luis.ild@gmail.com>
2011-08-08 14:57:37 +0000

    Borrando ficheros mal colocados, versiones finales a partir de ahora en directoiros compilationUnit

--- CocoNuestro/src/compilationunit/Main.java
+++ CocoNuestro/src/compilationunit/Main.java
@@ -1,14 +1,15 @@
+//Nombre del paquete
 package compilationunit;
 import java.io.Console;
 
 
 public class Main {
 	public static void main(String[] arg) {
-	Scanner scanner = new Scanner(arg[0]);
-	Parser parser = new Parser(scanner);
-	parser.Parse();
-	System.out.println(parser.errors.count + " errores detectados");
+		Scanner scanner = new Scanner(arg[0]);
+		Parser parser = new Parser(scanner);
+		parser.Parse();
+		System.out.println(parser.errors.count + " errores detectados");
 	}
-	}
+}
 
 
