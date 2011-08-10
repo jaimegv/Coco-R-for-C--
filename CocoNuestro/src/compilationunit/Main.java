08aa8cba6e03f660506244a56ab01c86144c2781
goarvic <goarvic@gmail.com>
2011-08-10 18:00:43 +0000

    Cambios en la estructura de las tablas de simbolos

--- CocoNuestro/src/compilationunit/Main.java
+++ CocoNuestro/src/compilationunit/Main.java
@@ -6,24 +6,14 @@ import java.io.Console;
 public class Main {
 	public static void main(String[] arg) {
 		int error;
-		TablaSimbolos tablaprueba = new TablaSimbolos();
-		Simbolo simboloprueba = new Simbolo("hola",1,1);
-		
-		error = tablaprueba.InsertarSimbolo(simboloprueba);
-		System.out.println(error);
+
 		
 		
-		Simbolo simboloprueba2 = new Simbolo ("hola",1 ,1);
-		
-		error = tablaprueba.InsertarSimbolo(simboloprueba2);
-		System.out.println(error);
-		
-		
-		/*
+
 		Scanner scanner = new Scanner(arg[0]);
 		Parser parser = new Parser(scanner);
 		parser.Parse();
-		System.out.println(parser.errors.count + " errores detectados");*/
+		System.out.println(parser.errors.count + " errores detectados");
 	}
 }
 
