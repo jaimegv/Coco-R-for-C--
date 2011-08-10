22a59fb6e9374413482523002ffbe60979263531
luisgs <luis.ild@gmail.com>
2011-08-10 18:35:46 +0000

    Añadido funciona para declarar nuevos ambitos en Tablas

--- CocoNuestro/src/compilationunit/Tablas.java
+++ CocoNuestro/src/compilationunit/Tablas.java
@@ -1,15 +1,23 @@
 package compilationunit;
-import java.util.Vector;
+//import java.util.Vector;
 
 public class Tablas {
 	
 	private TablaSimbolos Ambito_Global;
 	private TablaSimbolos Ambito_Actual;
 
-	
-	public Tablas()
-	{
+	// Constructor
+	public Tablas()	{
 		Ambito_Global = new TablaSimbolos();
 		Ambito_Actual = Ambito_Global;
 	}
+	
+	// Abrir un nuevo ámbito
+	public void NuevoAmbito(TablaSimbolos Actual) {
+		// Nuevo ambito a desarrollar que será el actual
+		// su padre es el actual ambito
+		TablaSimbolos Ambito = new TablaSimbolos(Ambito_Actual);
+		Ambito_Actual = Ambito;
+	}
+	
 }
