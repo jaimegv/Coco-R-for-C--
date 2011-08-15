a63d635bf2873627f51c2d6e8133b52a1758a35d
luisgs <luis.ild@gmail.com>
2011-08-15 10:35:34 +0000

    Nuevos ficheros de ejemplos para pruebas

--- CocoNuestro/fichPruebas/cout_pruebas.cpp
+++ CocoNuestro/fichPruebas/cout_pruebas.cpp
@@ -1,21 +1,5 @@
 void main () {
-cout << s << msg << f;
-	cout << "\n";	// imprime un salto de l�nea */
-	cout << d << "-" << m << "-" << a;
-	cout << nombre << ", con DNI n� " << numero << ", " << (masculino?"hombre":"mujer")
-	     << ", de " << edad () << "a�os de edad, naci� el: ";
-	cout << "Voy a imprimir los datos que he recopilado:";
-	cout << "Ahora voy a imprimir mis datos usando los m�todos:";
-	cout << yo.daNombre() << ", con DNI n� " << yo.numero << ", "
-   	  << (!yo.esMujer()?"hombre":"mujer") << ", de ";
-	cout << (2001-fecha.daAnno()) << "a�os de edad, naci� el ";
-	cout << "Escriba tres n�meros: ";
-		cout << s;
-		cout << v[i];	/* Imprime los elementos del vector al cuadrado */
-		cout << (zv[i]);	/* Imprime los elementos del vector al cuadrado */
-   cout << "\n";
-	cout << s;
-	cout << "\nIntroduce un n�mero";
-		case 0: cout << "El factorial de 0 siempre es 1.";
-				cout << "No existe el factorial de un negativo.";
+	cout << yo.daNombre() << ", con DNI n� " << yo.numero << ", ";
+
+	return hola;
 }
