57d5ec6139321e7ca6be9720fa25bbb5b93bbe1b
luisgs <luis.ild@gmail.com>
2011-08-23 12:07:40 +0000

    Nuevo Main.

--- CocoNuestro/src/compilationunit/Main.java
+++ CocoNuestro/src/compilationunit/Main.java
@@ -1,27 +1,23 @@
-//Nombre del paquete
 package compilationunit;
-//import java.io.Console;
-
 
 public class Main {
+	
 	public static void main(String[] arg) {
-		int error;
-		
-/*		Tablas tabla = new Tablas();
-		Simbolo simbolo = new Simbolo("hola", 0, 0);
-		
-		tabla.InsertarEnActual(simbolo);
-//		Simbolo simbolo_nuevo = new Simbolo("hola",0,0);
-		if (tabla.EstaEnActual("hola"))
-			System.out.println("El simbolo ya estaba");
-		else
-			System.out.println("uyuyuy");*/
-		
-		Scanner scanner = new Scanner(arg[0]);
-		Parser parser = new Parser(scanner);
-		parser.Parse();
-		System.out.println(parser.errors.count + " errores detectados");
-		
+
+		if (arg.length != 1) {	// Tenemos que recibir el fichero de codigo
+			System.err.println("Error: Se espera un fichero de código de entrada.");
+		} else {
+			// Se recibe fichero de codigo!
+			Scanner scanner = new Scanner(arg[0]);
+			Parser parser = new Parser(scanner);
+			parser.Parse();
+			if (parser.errors.count == 0) {	// todo ok!
+				System.out.println(parser.errors.count + " errores detectados.");
+				System.out.println("Fichero Codigo Objeto almacenado en \""+parser.fichero+"\"");				
+			} else {	// Errores detectados
+				System.err.println(parser.errors.count + " errores detectados.");	
+			}
+		}		
 	}
 }
 
