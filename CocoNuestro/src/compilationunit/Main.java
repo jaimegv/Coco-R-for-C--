30754f8a5d8cf19e460cbec99958b04acbe72e38
luisgs <luis.ild@gmail.com>
2011-08-25 11:37:47 +0000

    Modificacion argumentos de entrara Main, ahora necesita dos argumentos de entrada: fichero de codigo y fichero de salida para el codigo objeto

--- CocoNuestro/src/compilationunit/Main.java
+++ CocoNuestro/src/compilationunit/Main.java
@@ -4,13 +4,16 @@ public class Main {
 	
 	public static void main(String[] arg) {
 
-		if (arg.length != 1) {	// Tenemos que recibir el fichero de codigo
-			System.err.println("Error: Se espera un fichero de código de entrada.");
+		if (arg.length < 1) {	// Tenemos que recibir el fichero de codigo de entrada
+			System.err.println("Error: Se esperaba fichero de codigo de entrada.");
+		} else if (arg.length < 2) {	// Tenemos que recibir el fichero de salida
+			System.err.println("Error: Se esperaba fichero de salida.");			
 		} else {
-//			try {
-				// Se recibe fichero de codigo!
-				Scanner scanner = new Scanner(arg[0]);
-				Parser parser = new Parser(scanner);
+			try {
+				// Se recibe fichero de codigo de entrada!
+				Scanner scanner = new Scanner(arg[0]);	// lexico
+				Parser parser = new Parser(scanner);	// sintactico-seman
+				parser.fichero=arg[1];					// fichero de salida
 				parser.Parse();
 				if (parser.errors.count == 0) {	// todo ok!
 					System.out.println(parser.errors.count + " errores detectados.");
@@ -18,10 +21,10 @@ public class Main {
 				} else {	// Errores detectados
 					System.err.println(parser.errors.count + " errores detectados.");	
 				}
-//			} catch( Exception e ) {
+			} catch( Exception e ) {
 				// Captura de cualquier excepcion en ultima instancia
-//				System.err.println("¡Tranquilo vaquero!");
-//			}
+				System.err.println("¡Tranquilo vaquero!.Main");
+			}
 		}		
 	}
 }
