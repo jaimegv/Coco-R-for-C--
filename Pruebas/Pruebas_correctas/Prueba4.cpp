90d347ac919eff3a0bd7883476ea20d095e2b651
goarvic <goarvic@gmail.com>
2011-08-30 18:06:08 +0000

    Prueba4 mas completa

--- Pruebas/Pruebas_correctas/Prueba4.cpp
+++ Pruebas/Pruebas_correctas/Prueba4.cpp
@@ -1,3 +1,7 @@
+//Prueba en que se manipulan objetos locales y globales
+//modificando atributos públicos y llamando a métodos que manipulan
+//atributos privados
+
 class Persona
 	{
 	public:
@@ -20,20 +24,36 @@ int Persona :: GetSexo (void)
 	return sexo;
 	}
 
+Persona maria;
+
+
 void main ()
 	{
 	Persona juan;
-	juan.edad = 3;
+	juan.edad = 15;
 	juan.DNI = 32443;
 	juan.nombre = "Juan";
 	juan.SetSexo (1);
+	maria.edad = 16;
+	maria.DNI = 23334;
+	maria.nombre = "Maria";
+
 	if (juan.GetSexo() == 1)
 		{
 		cout << juan.nombre << " tiene una edad de "<< juan.edad << " anios, su DNI es " << juan.DNI << " y es hombre";
 		}
 	else
 		{
-		cout << juan.nombre << " tiene una edad de "<< juan.edad << " anios y su DNI es " << juan.DNI << " y es mujer";
+		cout << juan.nombre << " tiene una edad de "<< juan.edad << " anios, su DNI es " << juan.DNI << " y es mujer";
+		}
+
+	if (maria.GetSexo() == 1)
+		{
+		cout << maria.nombre << " tiene una edad de "<< maria.edad << " anios, su DNI es " << maria.DNI << " y es hombre";
+		}
+	else
+		{
+		cout << maria.nombre << " tiene una edad de "<< maria.edad << " anios, su DNI es " << maria.DNI << " y es mujer";
 		}
 	
 	}
