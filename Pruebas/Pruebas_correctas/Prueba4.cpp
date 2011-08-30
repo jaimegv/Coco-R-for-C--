77c1912a8ab72bde8062773751b274a161f3e92e
goarvic <goarvic@gmail.com>
2011-08-30 17:59:38 +0000

    Terminada la prueba4

--- Pruebas/Pruebas_correctas/Prueba4.cpp
+++ Pruebas/Pruebas_correctas/Prueba4.cpp
@@ -4,9 +4,21 @@ class Persona
 		int edad;
 		int DNI;
 		char* nombre;
+		void SetSexo(int);
+		int GetSexo();
 	private:
 		int sexo;
 	};
+
+void Persona :: SetSexo (int sexo_entrada)
+	{
+	sexo = sexo_entrada;
+	}
+
+int Persona :: GetSexo (void)
+	{
+	return sexo;
+	}
 
 void main ()
 	{
@@ -14,5 +26,14 @@ void main ()
 	juan.edad = 3;
 	juan.DNI = 32443;
 	juan.nombre = "Juan";
-	cout << juan.nombre << "tiene una edad de "<< juan.edad << " anios y su DNI es " << juan.DNI;
+	juan.SetSexo (1);
+	if (juan.GetSexo() == 1)
+		{
+		cout << juan.nombre << " tiene una edad de "<< juan.edad << " anios, su DNI es " << juan.DNI << " y es hombre";
+		}
+	else
+		{
+		cout << juan.nombre << " tiene una edad de "<< juan.edad << " anios y su DNI es " << juan.DNI << " y es mujer";
+		}
+	
 	}
